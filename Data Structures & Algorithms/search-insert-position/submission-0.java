e3a000e7c09e9e6f8ff9f4ffbcf8f67cce2a6cb1
Pawan Prasoon Mishra <34305913+PRINCE41@users.noreply.github.com>
2026-06-04 13:44:05 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int res = -1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target) return i;
+            else if(target < nums[i]) return i;
+            else if(target > nums[i]) res = i+1;
+        }
+        return res;
+    }
+}
