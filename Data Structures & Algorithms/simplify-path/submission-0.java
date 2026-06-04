b8f19da2f972cb77ed38ac98c82902dc9a3501ed
Pawan Prasoon Mishra <34305913+PRINCE41@users.noreply.github.com>
2026-06-04 13:05:59 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String simplifyPath(String path) {
+     Deque<String> stack = new ArrayDeque<>();
+
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+
+            if (part.isEmpty() || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            result.insert(0, "/" + stack.pop());
+        }
+
+        return result.toString();
+    }
+}
